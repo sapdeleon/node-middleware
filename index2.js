d66c5a3837153292dcb26900841096f2b57a375e
Samuel de Leon <sapdeleon@msn.com>
2024-12-19 16:01:16 +0800

    Initial upload

---
+++ index2.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
